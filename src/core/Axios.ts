e6f3cd0a83c66dd67788e146211847afce67cd76
pg243 <pg243911007@sina.cn>
2019-06-30 21:50:19 +0800

    完成第9章

---
+++ src/core/Axios.ts
@@ -0,0 +1,103 @@
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse, Method, RejectedFn, ResolvedFn } from '../types'
+import dispatchRequest from './dispatchRequest'
+import InterceptorManager from './interceptorManager'
+import mergeConfig from './mergeConfig'
+
+interface Interceptors {
+  request: InterceptorManager<AxiosRequestConfig>
+  response: InterceptorManager<AxiosResponse>
+}
+
+interface PromiseChain<T> {
+  resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosRequestConfig),
+  rejected?: RejectedFn
+}
+
+export default class Axios {
+  defaults: AxiosRequestConfig
+  interceptors: Interceptors
+
+  constructor(initConfig: AxiosRequestConfig) {
+    this.defaults = initConfig
+    this.interceptors = {
+      request: new InterceptorManager<AxiosRequestConfig>(),
+      response: new InterceptorManager<AxiosResponse>()
+    }
+  }
+
+  request(url: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
+    config = mergeConfig(this.defaults, config)
+    const chain: PromiseChain<any>[] = [{
+      resolved: dispatchRequest,
+      rejected: undefined
+    }]
+    this.interceptors.request.forEach((interceptor => {
+      chain.unshift(interceptor)
+    }))
+    this.interceptors.response.forEach((interceptor => {
+      chain.push(interceptor)
+    }))
+    let promise = Promise.resolve(config)
+    while (chain.length) {
+      const { resolved, rejected } = chain.shift()!
+      promise.then(resolved, rejected)
+    }
+    // return dispatchRequest(config)
+    return promise
+  }
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData('get', url, config)
+  }
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise {
+
+    return this._requestMethodWithoutData('delete', url, config)
+  }
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise {
+
+    return this._requestMethodWithoutData('head', url, config)
+  }
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise {
+
+    return this._requestMethodWithoutData('head', url, config)
+  }
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('put', url, data, config)
+  }
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('patch', url, data, config)
+  }
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithData('post', url, data, config)
+  }
+
+
+  _requestMethodWithoutData(method: Method, url: string, config?: AxiosRequestConfig) {
+    return this.request(Object.assign(config || {}, {
+      method,
+      url
+    }))
+  }
+
+  _requestMethodWithData(method: Method, url: string, data?: any, config?: AxiosRequestConfig) {
+    return this.request(Object.assign(config || {}, {
+      method,
+      url,
+      data
+    }))
+  }
+}
---
+++ src/defaults.ts
@@ -0,0 +1,21 @@
+import { AxiosRequestConfig } from './types'
+
+const defaults: AxiosRequestConfig = {
+  method: 'get',
+  timeout: 0,
+  headers: {
+    common: {
+      Accept: 'application/json, text/plain,*/*'
+    }
+  },
+  transformRequest: [],
+  transformResponse: []
+}
+const methodsNoData = ['delete', 'get', 'head', 'options']
+methodsNoData.forEach((method) => {
+  defaults.headers[method] = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})
+
+export default defaults
